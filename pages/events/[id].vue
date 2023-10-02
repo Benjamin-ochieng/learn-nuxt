bf4cec787ed4bd70fb9921f13b9dde81e6cc6e0f
BENJAMIN OCHIENG <benjaminochieng@Latitude-7490-489c7a3c.localdomain>
2023-10-02 07:43:46 +0300

    initial commit

---
+++ pages/events/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Event number {{ pageId }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import { useRoute } from "vue-router";
+
+const route = useRoute();
+const pageId = route.params.id;
+
+</script>
+
+<style scoped></style>
---
+++ pages/events/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ul>
+        <li>
+            <NuxtLink to="/events/1">Event 1</NuxtLink>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
